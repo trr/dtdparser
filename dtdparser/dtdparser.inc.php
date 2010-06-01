4f841a102319b6054ae8c91b7d71e319d0b6c903
Thomas Rutter <trutter@pack>
2010-06-02 00:23:48 +1000

    dtdparser: update - null cannot be used for empty elements because isset() is false for null

--- dtdparser/dtdparser.inc.php
+++ dtdparser/dtdparser.inc.php
@@ -78,7 +78,7 @@ class dtdparser
 	
 	private function translateelementname($name)
 	{
-		if ($name == 'EMPTY') return null;
+		if ($name == 'EMPTY') return '';
 		if ($name == '#PCDATA') return '#text';
 		if ($name == 'CDATA') return '#cdata-section';
 		if (preg_match('#^[a-zA-Z:_]#', $name)) return strtolower($name);
